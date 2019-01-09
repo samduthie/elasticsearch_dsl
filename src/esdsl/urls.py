59516e69ab12b19ae3e37533493abdd9d6467686
Sam Duthie <Samduthie@gmail.com>
2019-01-09 01:47:26 +0000

    Add in needed pip modules

--- src/esdsl/urls.py
+++ src/esdsl/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from pokedex import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^pokemon/', include('pokedex.urls')),
+    url(r'^admin', admin.site.urls),
+    url(r'^pokemon', include('pokedex.urls')),
     url(r'^', views.random_pokemon_image),
 ]
